f5144f4089c3e4b25cde9ed5b89547835193c06b
Bogdan <noskovbogdan@gmail.com>
2022-02-08 15:54:37 +0300

    feat: update games component

--- src/ts/components/games/games.ts
+++ src/ts/components/games/games.ts
@@ -1,12 +1,50 @@
+import ServerAPI from '../../serverAPI';
+import Sprint from './sprint/sprint';
+import AudioCall from './audioCall/audioCall';
+import GameResult from './gameResult/gameResult';
+
 export default class Games {
   innerHtmlTemplate = `
   <h1>Games</h1>
+  <label>
+    <input type="radio" name="gameName" value="sprint" checked>
+    Спринт
+  </label>
+  <label>
+    <input type="radio" name="gameName" value="audioCall">
+    Аудиовызов
+  </label>
+  <select class="games__select" name="gameLevel">
+    <option value="0">Level 1</option>
+    <option value="1">Level 2</option>
+    <option value="2">Level 3</option>
+    <option value="3">Level 4</option>
+    <option value="4">Level 5</option>
+    <option value="5">Level 6</option>
+  </select>
+  <button class="games__start">Начать</button>
   `;
+
+  serverAPI: ServerAPI;
 
   componentElem: HTMLElement;
 
-  constructor() {
+  gameResult: GameResult;
+
+  sprint: Sprint;
+
+  audioCall: AudioCall;
+
+  contentURL: string;
+
+  constructor(serverAPI: ServerAPI, contentURL: string) {
     this.componentElem = document.createElement('div');
+    this.serverAPI = serverAPI;
+    this.contentURL = contentURL;
+
+    this.gameResult = new GameResult(this.contentURL);
+    this.sprint = new Sprint(this.serverAPI, this.contentURL, this.gameResult);
+    this.audioCall = new AudioCall(this.serverAPI, this.contentURL, this.gameResult);
   }
 
   createThisComponent() {
@@ -16,13 +54,52 @@ export default class Games {
 
   createComponent() {
     this.createThisComponent();
+    this.gameResult.createComponent();
+    this.sprint.createComponent();
+    this.audioCall.createComponent();
+  }
+
+  setThisListeners() {
+    this.listenerForStartBtn();
+  }
+
+  setListeners() {
+    this.setThisListeners();
+    this.gameResult.setListeners();
   }
 
   showComponent = () => {
-    document.body.querySelector('.header')?.after(this.componentElem);
+    const contentElem = document.querySelector('.content') as HTMLElement;
+    contentElem.append(this.componentElem);
   };
 
   hideComponent = () => {
     document.querySelector('.games')?.remove();
   };
+
+  getGameInfo() {
+    const gameNameInput = this.componentElem.querySelector(
+      'input[name="gameName"]:checked'
+    ) as HTMLInputElement;
+    const gameLevelSelect = this.componentElem.querySelector(
+      'select[name="gameLevel"]'
+    ) as HTMLSelectElement;
+
+    const gameName = gameNameInput.value;
+    const gameLevel = +gameLevelSelect.value;
+    return [gameName, gameLevel];
+  }
+
+  listenerForStartBtn() {
+    const startBtn = this.componentElem.querySelector('.games__start') as HTMLButtonElement;
+    startBtn.addEventListener('click', () => {
+      const [gameName, gameLevel] = this.getGameInfo();
+
+      if (gameName === 'sprint') {
+        this.sprint.startGame(+gameLevel);
+      } else if (gameName === 'audioCall') {
+        this.audioCall.startGame(+gameLevel);
+      }
+    });
+  }
 }
