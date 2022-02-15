292b7cd4123b96868471b79df6ab0f64fff87deb
aytsaN <aytsan.shamal@gmail.com>
2022-02-15 04:15:13 +0300

    feat: add styles for game start page

--- src/ts/components/games/games.ts
+++ src/ts/components/games/games.ts
@@ -10,25 +10,27 @@ export default class Games {
         <input type="radio" name="gameName" value="sprint" id="sprintRadio">
         <input type="radio" name="gameName" value="audioCall" id="audioCallRadio">
         <label for="sprintRadio">
-          <h3>Спринт</h3>
           <img class="games__image games__image_simple" src="./assets/svg/sprint-game.svg" alt="Спринт">
           <img class="games__image games__image_color" src="./assets/svg/sprint-game-color.svg" alt="Спринт">
+          <h3 class="games__name">Спринт</h3>
         </label>
+        <div class="games__controls-container">
+        <button class="games__start" disabled>Начать</button>
+        <select class="games__select" name="gameLevel">
+          <option value="0">Уровень 1</option>
+          <option value="1">Уровень 2</option>
+          <option value="2">Уровень 3</option>
+          <option value="3">Уровень 4</option>
+          <option value="4">Уровень 5</option>
+          <option value="5">Уровень 6</option>
+        </select>
+      </div>
         <label for="audioCallRadio">
-          <h3>Аудиовызов</h3>
           <img class="games__image games__image_simple" src="./assets/svg/audioCall-game.svg" alt="Аудиовызов">
           <img class="games__image games__image_color" src="./assets/svg/audioCall-game-color.svg" alt="Аудиовызов">
+          <h3 class="games__name">Аудиовызов</h3>
         </label>
       </div>
-      <select class="games__select" name="gameLevel">
-        <option value="0">Level 1</option>
-        <option value="1">Level 2</option>
-        <option value="2">Level 3</option>
-        <option value="3">Level 4</option>
-        <option value="4">Level 5</option>
-        <option value="5">Level 6</option>
-      </select>
-      <button class="games__start" disabled>Начать</button>
     </div>
   `;
 
@@ -103,6 +105,7 @@ export default class Games {
     gameRadioInputContainer.addEventListener('click', () => {
       const radioBtns = gameRadioInputContainer.querySelectorAll<HTMLInputElement>('input');
       const radioBtnsArr = Array.from(radioBtns);
+
       if (radioBtnsArr.some((el) => el.checked)) {
         const startBtn = this.componentElem.querySelector('.games__start') as HTMLButtonElement;
         startBtn.removeAttribute('disabled');
